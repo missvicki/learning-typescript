377f661682de487ddaedadaefeb105b2743041b0
Victor Nomwesigwa <bethel@Bethel.local>
2019-09-23 15:50:03 +0300

    complex objects

--- app.ts
+++ app.ts
@@ -88,3 +88,11 @@ let userData: { name: string; age: number } = {
 //     a: "hello",
 //     b: 22
 // } error properties don't match name and age
+
+// complex object
+let complex: { data: number[]; output: (all: boolean) => number[] } = {
+  data: [100, 3, 10],
+  output: function(all: boolean): number[] {
+    return this.data;
+  }
+};
