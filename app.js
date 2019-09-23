54551e3ba3ac75350d402ea7ca1a56c51fb1374f
Victor Nomwesigwa <bethel@Bethel.local>
2019-09-23 14:59:12 +0300

    arrays and tuples

--- app.js
+++ app.js
@@ -2,12 +2,23 @@
 // string
 var myName = "Max";
 // myName = 2; error
+var ourName = "Vicki";
 // number
 var myAge = 27;
 // myAge = "Max"; error
 // boolean
-var haHobbies = false;
+var hasHobbies = false;
 // hasHobbies = 1; error
 // assign types
 var myRealAge;
 myRealAge = 27;
+var ourAge;
+ourAge = 2;
+// array
+var hobbies = ["Cooking", "Sports"];
+console.log(hobbies[0]);
+console.log(typeof hobbies);
+// hobbies = [100]; error
+hobbies = ["a string"];
+var hobby = ["hey"];
+hobby = [1];
--- app.ts
+++ app.ts
@@ -10,7 +10,7 @@ let myAge = 27;
 // myAge = "Max"; error
 
 // boolean
-let haHobbies = false;
+let hasHobbies = false;
 // hasHobbies = 1; error
 
 // assign types
@@ -19,3 +19,18 @@ myRealAge = 27;
 
 let ourAge: number;
 ourAge = 2;
+
+// array
+let hobbies = ["Cooking", "Sports"];
+console.log(hobbies[0]);
+console.log(typeof hobbies);
+// hobbies = [100]; error
+hobbies = ["a string"];
+
+let hobby: any[] = ["hey"];
+hobby = [1];
+// hobby = 100 error
+
+// tuples
+let address: [string, number] = ["Superstreet", 99];
+// let address: [string, number] = [99, "Superstreet"]; //error
